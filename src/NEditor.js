60a310232d1c0645cf798e9eaeca14ac04ce8040
Xin Cheng <xin.cheng.9611@gmail.com>
2021-01-04 23:15:30 +0100

    add id for div elements

--- src/NEditor.js
+++ src/NEditor.js
@@ -302,6 +302,7 @@ NEditor.Node = function (sTitle) {
 	this.eRoot = document.createElement("div");
 	document.body.appendChild(this.eRoot);
 	this.eRoot.className = "NodeContainer";
+	this.eRoot.id = "newNodeContainer";
 	this.eRoot.ref = this;
 
 	//.........................
@@ -342,11 +343,14 @@ NEditor.Node.prototype.addChild = function (type, text, id) {
 
 var op_replicaKey = function replicateKeyGroupSet(keyGroupId, kgx, kgy, rnx, rny, rn1x, rn1y, rn2x, rn2y, clientNode) {
 	var n4 = new NEditor.Node("Root Replica Node");
+	document.getElementById("newNodeContainer").id = keyGroupId+"RootReplica"
 	var n4i1 = n4.addInput("Node Name: \"nodeA\"");
 	var n4i2 = n4.addInput("Root Address:\"172.26.1.1:9001\"");
 	n4.setPosition(rnx, rny);
+	n4.Title
 
-	var n3 = new NEditor.Node(keyGroupId);
+	var n3 = new NEditor.Node("Key Group "+keyGroupId);
+	document.getElementById("newNodeContainer").id = keyGroupId+"KeyGroup"
 	var n3i1 = n3.addInput("Name (configurable)");
 	var n3o1 = n3.addOutput("Mutable (configurable)");
 	var n3o1 = n3.addOutput("Expire (configurable)");
@@ -354,11 +358,13 @@ var op_replicaKey = function replicateKeyGroupSet(keyGroupId, kgx, kgy, rnx, rny
 	n3.setWidth(250);
 
 	var n5 = new NEditor.Node("Replica Node 1");
+	document.getElementById("newNodeContainer").id = keyGroupId+"ReplicaNode1"
 	var n5i1 = n5.addInput("Node Name: \"nodeB\"");
 	var n5o1 = n5.addInput("Expire (this should be configurable)");
 	n5.setPosition(rn1x, rn1y);
 
-	var n2 = new NEditor.Node("Replica Node 3");
+	var n2 = new NEditor.Node("Replica Node 2");
+	document.getElementById("newNodeContainer").id = keyGroupId+"ReplicaNode2"
 	var n2i1 = n2.addInput("Node Name: \"nodeC\"");
 	var n2o1 = n2.addInput("Expire (this should be configurable)");
 	n2.setPosition(rn2x, rn2y);
